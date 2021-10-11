89d5953f1820a16dcd99277b94dffd2a8952a3c7
Matthieu Dumont <jerska@live.fr>
2021-10-11 18:24:37 +0200

    feat(dev): add prettier

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const express = require('express');
 
 const unsplash_credentials = require('./unsplash/credentials.js');
 
-const PORT = parseInt(process.env.PORT || "3000", 10);
+const PORT = parseInt(process.env.PORT || '3000', 10);
 const UNSPLASH_ACCESS_TOKEN = process.env.UNSPLASH_ACCESS_TOKEN;
 const UNSPLASH_SECRET_KEY = process.env.UNSPLASH_SECRET_KEY;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).send('Success');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
