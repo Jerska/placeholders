f6bf33f5793f31d1f6956b93266a3c54a063184c
Matthieu Dumont <jerska@live.fr>
2021-10-11 18:43:29 +0200

    fix(credentials): only use access key

--- unsplash/credentials.js
+++ unsplash/credentials.js
@@ -1,15 +1,9 @@
-const UNSPLASH_ACCESS_TOKEN = process.env.UNSPLASH_ACCESS_TOKEN;
-const UNSPLASH_SECRET_KEY = process.env.UNSPLASH_SECRET_KEY;
+const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
-if (!UNSPLASH_ACCESS_TOKEN || UNSPLASH_ACCESS_TOKEN === 'FIXME') {
-  throw new Error('Missing UNSPLASH_ACCESS_TOKEN');
-}
-
-if (!UNSPLASH_SECRET_KEY || UNSPLASH_SECRET_KEY === 'FIXME') {
-  throw new Error('Missing UNSPLASH_SECRET_KEY');
+if (!UNSPLASH_ACCESS_KEY || UNSPLASH_ACCESS_KEY === 'FIXME') {
+  throw new Error('Missing UNSPLASH_ACCESS_KEY');
 }
 
 module.exports = {
-  access_token: UNSPLASH_ACCESS_TOKEN,
-  secret_key: UNSPLASH_SECRET_KEY,
+  access_key: UNSPLASH_ACCESS_KEY,
 };
